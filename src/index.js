de3911f7ce328dea6b006febaf71f126e8dcfdcd
Jose <joseluis0528@gmail.com>
2023-04-11 22:51:11 -0500

    added cards

--- src/index.js
+++ src/index.js
@@ -14,12 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function displayToys() {
+function fetchToys() {
   fetch(`http://localhost:3000/toys`)
   .then(response => response.json())
-  .then(data => {
-    
-  })
+  .then(data => renderToys(data))
 }
 
-const card = document.getElementsByClassName('container')
+function renderToys(toys) {
+  const card = document.querySelector('.container');
+  toys.forEach(toy => {
+    const div = document.createElement('div');
+    div.className = 'card';
+    card.append(div);
+    const h2 = document.createElement('h2');
+    h2.textContent = toy.name;
+    const img = document.createElement('img');
+    img.src = toy.image;
+    img.className = 'toy-avatar';
+    const p = document.createElement('p');
+    p.textContent = `${toy.likes} likes`;
+    const button = document.createElement('button');
+    button.textContent = 'Like';
+    button.className = 'like-btn';
+    button.id = toy.id;
+    div.append(h2, img, p, button);
+  })
+}
